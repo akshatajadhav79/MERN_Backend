14f38affa425e88435e9cc491dafd4821c4f2f22
akshata <akshatajadhav7902@gmail.com>
2025-10-17 21:41:17 +0530

    add changes in mongodb connection string useNewUrlParser and useUnifiedTopology

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const connect_db = ()=>{
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGO_URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(()=>{console.log("DB Connection..!")})
     .catch((err)=>{
         console.error("Error DB:",err.message);
